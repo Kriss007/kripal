1accaa623058281a3b5a979e309315eb3ab63316
kriss007 <kriss4050@gmail.com>
2019-10-05 12:35:34 +0530

    first commit

---
+++ src/main/java/com/kripal1/kripal/KripalApplication.java
@@ -0,0 +1,13 @@
+package com.kripal1.kripal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KripalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KripalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kripal1/kripal/KripalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kripal1.kripal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KripalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
